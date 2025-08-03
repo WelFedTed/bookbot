42c89cea7bdc668a66763fef7cfffbb9615cfaad
WelFedTed <34222508+WelFedTed@users.noreply.github.com>
2025-08-04 06:59:33 +1000

    Count Characters

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from stats import get_num_words
+from stats import get_char_list
 
 def get_book_text(book):
     file = open(book)
     return file.read()
 
 def main():
-    num_words = get_num_words(get_book_text("books/frankenstein.txt"))
+    book = get_book_text("books/frankenstein.txt")
+    num_words = get_num_words(book)
     print(f"{num_words} words found in the document")
+    char_list = get_char_list(book)
+    print(char_list)
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,12 @@
 def get_num_words(book_text):
     return len(book_text.split())
+
+def get_char_list(book_text):
+    text = book_text.lower()
+    char_list = {}
+    for char in text:
+        if char not in char_list:
+            char_list[char] = 1
+            continue
+        char_list[char] += 1
+    return char_list
