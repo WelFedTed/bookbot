9969b6453c1c4d2d0b53c90cceecf114b439ac04
WelFedTed <34222508+WelFedTed@users.noreply.github.com>
2025-08-04 06:42:07 +1000

    Count Words

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
+from itertools import count
 def get_book_text(book):
-    # while open(book) as f:
-    #     file_contents = f.read()
-    #     return file_contents
     file = open(book)
     return file.read()
 
+def count_book_words(book_text):
+    return len(book_text.split())
 
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    num_words = count_book_words(get_book_text("books/frankenstein.txt"))
+    print(f"{num_words} words found in the document")
 
 main()
